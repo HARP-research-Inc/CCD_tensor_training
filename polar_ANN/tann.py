074274b208e5730dd5952f22e0a50b36f9b408ab
tdou25 <thomasyd1120@gmail.com>
2025-03-28 12:34:19 -0400

    added TANN outline

--- polar_ANN/tann.py
+++ polar_ANN/tann.py
@@ -1,6 +1,19 @@
 class TANN:
     """
-        Triangulation-Approximate-Nearest-Neighbor algorithm
+        Triangulation-Approximate-Nearest-Neighbor algorithm?
+        Support-Point_ANN?
+
+        Idk tbh the name is TBA.
     """
-    def __init__(self):
+    def __init__(self, K: int, C: int):
+        self.__top_array: list = None
+        self.__pointer_lists = list()
+        self.__K = K
+        self.__C = C
+
+    def build():
         pass
+    def query():
+        pass
+
+    
