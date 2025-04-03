1aaf08e7a5d7108303a7d45ca215d1f6f1381797
Thomas Dou <thomasyd1120@gmail.com>
2025-04-03 15:45:58 -0400

    added one verb class

--- transitive_build_embeddings.py
+++ transitive_build_embeddings.py
@@ -5,9 +5,53 @@ from sentence_transformers import SentenceTransformer
 import os
 import json
 import joblib  # For saving the PCA model
+from util import get_embedding_in_parallel
 
 def build_trans(data, BERT_model, FT):
     
+    num_nouns = 50
+    num_verbs = len(data)
+    pca = PCA(n_components=300)
+
+    print("Number of verbs: ", num_verbs)
+
+    empirical_embeddings = torch.zeros((num_verbs*num_nouns*num_nouns, 384))
+    
+    s_o_embeddings = list()
+
+    for v_i, verb in enumerate(data):
+        #print("v_i: ", v_i)
+        nouns = data[verb]
+
+        subjects = nouns[0]
+        objects = nouns[1]        
+        
+        for s_i, subject in enumerate(subjects, start=0):
+            for o_i, object in enumerate(objects, start=0):
+                #print(s_i, o_i)
+                sentence = subject + " " + verb + " " + object
+
+                #tokenize
+
+                #get sentence embedding
+                sentence_embedding = model.encode(sentence, convert_to_tensor=True)
+                #get fasttext s/o embeddings
+                subject_embedding = torch.Tensor(ft_model[subject])
+                object_embedding = torch.Tensor(ft_model[object])
+            
+                s_o_embeddings.append((subject_embedding, object_embedding))
+
+                print(sentence_embedding.shape)
+                empirical_embeddings[v_i*num_nouns*num_nouns + s_i*num_nouns + o_i] = sentence_embedding
+                
+    os.makedirs("data", exist_ok=True)
+
+    empirical_embeddings = pca.fit_transform(empirical_embeddings)
+
+    return pca, empirical_embeddings, s_o_embeddings
+
+
+def build_one_verb(verb, BERT_model):
     num_nouns = 50
     num_verbs = len(data)
     pca = PCA(n_components=300)
