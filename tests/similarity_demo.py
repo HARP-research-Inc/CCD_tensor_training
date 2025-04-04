bff97e8e654278f9a01bb9b902eecff2d7cd96a2
Thomas Dou <thomasyd1120@gmail.com>
2025-04-04 11:20:41 -0400

    wrote big verb model

--- tests/similarity_demo.py
+++ tests/similarity_demo.py
@@ -11,19 +11,19 @@ from util import cosine_sim, get_embedding_in_parallel, API_query_embedding, gen
 
 
 if __name__ == "__main__":
-    file = open("../data/test_sentences.txt", 'r')
+    #file = open("../data/test_sentences.txt", 'r')
 
     #loading embedding models
     model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
     #ft_model = api.load('fasttext-wiki-news-subwords-300')
 
     tensor_function = FullRankTensorRegression(300, 300)
-    tensor_function.load_state_dict(torch.load("../adj_weights_on_the_fly.pt"))
+    tensor_function.load_state_dict(torch.load("../models/hybrid_weights_dummy"))
 
     tensor_function.eval()
     
     #loading transform model
-    pca = joblib.load("../data/adj_pca_model.pkl")
+    pca = joblib.load("../data/pca_model.pkl")
 
     expected1 , actual_sentence_embedding1 = API_query_embedding("big guy", pca, model, tensor_function, pos="adjective")    
     expected2 , actual_sentence_embedding2 = API_query_embedding("large man", pca, model, tensor_function, pos="adjective")
