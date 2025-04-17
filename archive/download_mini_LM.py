93876e5d175a46ee8b749cee45367b991bb202ea
Thomas Dou <thomasyd1120@gmail.com>
2025-04-17 15:36:13 -0400

    added trash

--- archive/download_mini_LM.py
+++
@@ -1,6 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-# Load and download the model automatically
-model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-
-print("Model downloaded successfully!")
