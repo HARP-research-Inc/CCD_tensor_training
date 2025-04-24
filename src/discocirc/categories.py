0af1c6c386b8e427df734fa40fe65f1c3a655b69
Thomas Dou <thomasyd1120@gmail.com>
2025-04-24 16:29:29 -0400

    adding framework related related files

---
+++ src/discocirc/categories.py
@@ -0,0 +1,42 @@
+import torch
+
+class Category(object):
+    def __init__(self, label):
+        self.label = label
+    def forward(self):
+        """
+        """
+        return self
+    def inward(other):
+        """
+        """
+        return other
+
+class Circuit(Category):
+    """
+
+    """
+    def __init__():
+        pass
+
+class Wire(Category):
+    def __init__():
+        pass
+
+class Box(Category):
+    def __init__():
+        pass
+
+class Spider(Category):
+    def __init__():
+        pass
+
+class Actor(Wire):
+    def __init__():
+        pass
+    def forward():
+        pass
+
+
+if __name__ == "__main__":
+    print("test")
